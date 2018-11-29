c3885cc73dd8685f0a81189117cc46b1b1788e84
Misha <misha@gomisha.com>
2018-11-29 16:24:14 -0500

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -73,7 +73,7 @@ def create_pipeline(project_url, pipeline_token, ref, variables={}) -> Optional[
         f'{project_url}/trigger/pipeline',
         data=data
     )
-    print("hello world")
+    print("DEBUG: project_url" + project_url)
     assert r.status_code == 201, f'Failed to create pipeline, api returned status code {r.status_code}'
     pid = r.json().get('id', None)
     print(f'Pipeline created (id: {pid})')
